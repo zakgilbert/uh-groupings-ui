fa4e07e4619892bfd3fd40fd80691c37fceb2fba
zakgilbert <gilbertz@hawaii.edu>
2019-07-12 13:57:26 -1000

    ...

--- src/main/resources/static/javascript/mainApp/general.controller.js
+++ src/main/resources/static/javascript/mainApp/general.controller.js
@@ -67,7 +67,7 @@
         var maxLength = 100;
         var noDescriptionMessage = "No description given for this Grouping.";
 
-        angular.extend(this, $controller("TableJsController", {$scope: $scope}));
+        angular.extend(this, $controller("TableJsController", { $scope: $scope }));
 
         /**
          * Initiates the retrieval of information about the grouping clicked by the user.
@@ -191,7 +191,7 @@
 
                 const syncDestResponseMapping = new Map(Object.entries(res.syncDestinations));
                 syncDestResponseMapping.forEach((value, key, map) => {
-                    $scope.syncDestArray.push({name: key, value: value});
+                    $scope.syncDestArray.push({ name: key, value: value });
                 });
                 $scope.setSyncDestLabels();
 
@@ -267,7 +267,7 @@
                 } else if (res.statusCode === 403) {
                     $scope.createOwnerErrorModal();
                 } else {
-                    dataProvider.handleException({exceptionMessage: res.exceptionMessage}, "feedback/error", "feedback");
+                    dataProvider.handleException({ exceptionMessage: res.exceptionMessage }, "feedback/error", "feedback");
                 }
             });
         };
@@ -376,7 +376,7 @@
         $scope.addInBasis = function (group) {
             _.forEach(group, function (member) {
                 const memberUuid = member.uuid;
-                member.inBasis = _.some($scope.groupingBasis, {uuid: memberUuid})
+                member.inBasis = _.some($scope.groupingBasis, { uuid: memberUuid })
                     ? "Yes"
                     : "No";
             });
@@ -391,12 +391,12 @@
             _.forEach(compositeGroup, function (member) {
 
                 const memberUuid = member.uuid;
-                if (_.some($scope.groupingBasis, {uuid: memberUuid})) {
+                if (_.some($scope.groupingBasis, { uuid: memberUuid })) {
 
                     member.whereListed = "Basis";
                 }
 
-                if (_.some($scope.groupingInclude, {uuid: memberUuid})) {
+                if (_.some($scope.groupingInclude, { uuid: memberUuid })) {
                     member.whereListed = _.isUndefined(member.whereListed)
                         ? "Include"
                         : "Basis / Include";
@@ -410,8 +410,10 @@
          */
         $scope.addMember = function (list) {
             const groupingPath = $scope.selectedGrouping.path;
+            console.log(list);
             groupingsService.getGrouping(groupingPath, 1, PAGE_SIZE, "name", true, function () {
                 const userToAdd = $scope.userToAdd;
+                console.log(userToAdd);
                 if (_.isEmpty(userToAdd)) {
                     $scope.createAddErrorModal(userToAdd);
                 } else if ($scope.existInList(userToAdd, list)) {
@@ -444,7 +446,21 @@
             });
             console.log(usersToAdd);
         };
+        /**
+        $scope.openFile = function (event) {
+            let input = event.target;
+            let reader = new FileReader();
 
+            reader.onload = function () {
+                let text = reader.resault;
+                console.log(reader.result.substring(0, 200));
+            };
+            reader.readAsText(input.files[0]);
+        };
+         **/
+        $scope.importMembers = function (list) {
+            let input = $scope.event.target;
+        };
         /**
          * Creates a modal display for members added, and calls addMembersToInclude service.
          * @param usersToAdd
@@ -453,7 +469,6 @@
         $scope.updateAddMembers = function (usersToAdd, list) {
 
             let groupingPath = $scope.selectedGrouping.path;
-
             let handleSuccessfulAdd = function (res, list, usersToAdd) {
                 $scope.createSuccessfulAddModal({
                     user: usersToAdd,
@@ -507,9 +522,9 @@
          */
         $scope.isInAnotherList = function (user, list) {
             if (list === "Include") {
-                return _.some($scope.groupingExclude, {username: user});
+                return _.some($scope.groupingExclude, { username: user });
             } else if (list === "Exclude") {
-                return _.some($scope.groupingInclude, {username: user});
+                return _.some($scope.groupingInclude, { username: user });
             }
             return false;
         };
@@ -521,9 +536,9 @@
          */
         $scope.existInList = function (user, list) {
             if (list === "Include") {
-                return _.some($scope.groupingInclude, {username: user});
+                return _.some($scope.groupingInclude, { username: user });
             } else if (list === "Exclude") {
-                return _.some($scope.groupingExclude, {username: user});
+                return _.some($scope.groupingExclude, { username: user });
             }
             return false;
         };
@@ -1022,7 +1037,7 @@
          */
         $scope.getSyncDestValueInArray = function (syncDestName) {
             const indexOfSyncDest = $scope.syncDestArray.map((e) => {
-                return e.name
+                return e.name;
             }).indexOf(syncDestName);
             const syncDestOn = $scope.syncDestArray[indexOfSyncDest].value;
             return syncDestOn;
@@ -1035,7 +1050,7 @@
          */
         $scope.getEntireSyncDestInArray = function (syncDestName) {
             const indexOfSyncDest = $scope.syncDestArray.map((e) => {
-                return e.name
+                return e.name;
             }).indexOf(syncDestName);
             return $scope.syncDestArray[indexOfSyncDest];
         };
@@ -1047,7 +1062,7 @@
          */
         $scope.setSyncDestInArray = function (syncDestName, syncDestvalue) {
             const indexOfSyncDest = $scope.syncDestArray.map((e) => {
-                return e.name
+                return e.name;
             }).indexOf(syncDestName);
             $scope.syncDestArray[indexOfSyncDest].value = syncDestvalue;
         };
@@ -1062,7 +1077,7 @@
             const syncDestOn = $scope.getSyncDestValueInArray(syncDestName);
 
             groupingsService.setSyncDest(groupingPath, syncDestName, syncDestOn, handleSuccessfulPreferenceToggle, handleUnsuccessfulRequest);
-        }
+        };
 
         /**
          * Creates a modal indicating an error in saving the grouping's preferences.
